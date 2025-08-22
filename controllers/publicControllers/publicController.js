56418356ace40ac722b2e18c440dc75c14e6b957
soumyaranjan-dev <ranjansoumya33479@gmail.com>
2025-08-22 23:19:57 +0530

    do last of 23:19

--- controllers/publicControllers/publicController.js
+++ controllers/publicControllers/publicController.js
@@ -1,9 +1,12 @@
+const USER = require("../../models/userModel/userModel")
+const jwttoken = require("jsonwebtoken")
+
 const defaultPublic = (req, res) => {
     res.json({ message: "default public route" })
 }
 
 const signupPublic = async (req, res) => {
-    const userData = req.body
+    const userRegData = req.body
     const { userEmail } = userData
     try {
         const exists = await USER.findOne({ userEmail })
@@ -11,12 +14,24 @@ const signupPublic = async (req, res) => {
         await USER.create(userData)
         res.status(200).json({message: "Good Signup!"})
     } catch (error) {
+        console.log(error)
         res.status(500).json({message: "Internal Error!"})
     }
 }
 
-const signinPublic = (req, res) => {
-    res.json({ message: "signin route" })
+const signinPublic = async (req, res) => {
+    const userLogData = req.body
+    const {userEmail} = userLogData
+    try {
+        const exists = await USER.findOne({ userEmail })
+        if (!exists) return res.status(201).json({ message: "User not Exists! Try Another" })
+        
+        const token = jwttoken.sign({userEmail}, process.env.PRIVATE_KEY,{expiresIn: "1hr"})
+        res.status(200).json({ message: "Good Signin!", token })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Internal Error!" })
+    }
 }
 
 module.exports = {defaultPublic, signupPublic, signinPublic}
